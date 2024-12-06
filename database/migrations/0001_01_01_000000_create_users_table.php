e82505895c848393f7b4f36281a510067e75b263
Artyom-exe <thlambot@hotmail.fr>
2024-12-06 16:24:58 +0100

    table user modif

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,11 +17,28 @@ return new class extends Migration
             $table->string('password');
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
-            $table->enum('role', ['admin', 'technicien, client']);
+            $table->enum('role', ['admin', 'technicien', 'client']);
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
+
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
+        });
     }
+
+
 
     /**
      * Reverse the migrations.
@@ -29,5 +46,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('utilisateurs');
+        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('sessions');
     }
 };
